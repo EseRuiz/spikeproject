3fd65d2d2505f546bb134f3ff0683afe913f4e73
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2023-10-20 06:52:00 -0500

    Delete spike directory

--- spike/enterprise/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from enterprise.viewsets import EnterpriseViewSet
-
-router=routers.SimpleRouter()
-router.register('empresa',EnterpriseViewSet)
-urlpatterns = router.urls
